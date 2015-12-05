3a2df593ff7476ac5383d80f3827eeae3b6e1e74
Robert Abeyta <robertabeyta@outlook.com>
2015-12-05 07:09:50 -0700

    initial gen app with gitignore

---
+++ src/main/java/me/abeyta/deckmanager/DeckManagerApplication.java
@@ -0,0 +1,12 @@
+package me.abeyta.deckmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeckManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeckManagerApplication.class, args);
+    }
+}
---
+++ src/test/java/me/abeyta/deckmanager/DeckManagerApplicationTests.java
@@ -0,0 +1,18 @@
+package me.abeyta.deckmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DeckManagerApplication.class)
+@WebAppConfiguration
+public class DeckManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
