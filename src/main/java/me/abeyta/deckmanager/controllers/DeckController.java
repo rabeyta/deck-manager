a63250744e4bf77a3db6dc396c822e683477a8d6
Robert Abeyta <robertabeyta@outlook.com>
2015-12-05 08:33:30 -0700

    added produces header and created status as output from create deck method

--- src/main/java/me/abeyta/deckmanager/controllers/DeckController.java
+++ src/main/java/me/abeyta/deckmanager/controllers/DeckController.java
@@ -1,9 +1,12 @@
 package me.abeyta.deckmanager.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.abeyta.deckmanager.delegates.DeckManager;
@@ -16,7 +19,8 @@ public class DeckController {
 	@Autowired
 	private DeckManager manager;
 	
-	@RequestMapping(value="/{deckName}", method=RequestMethod.PUT)
+	@RequestMapping(value="/{deckName}", method=RequestMethod.PUT, produces=MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
 	public Deck createNewDeck(@PathVariable(value="deckName") String deckName) {
 		return manager.create(deckName);
 	}
