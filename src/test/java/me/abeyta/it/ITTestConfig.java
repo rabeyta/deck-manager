ff20e8a749730d07d757fea23cb86bac39d6610b
Robert Abeyta <robertabeyta@outlook.com>
2015-12-06 07:25:41 -0700

    added logic to use property for port instead of default 8080 when it is set

--- src/test/java/me/abeyta/it/ITTestConfig.java
+++ src/test/java/me/abeyta/it/ITTestConfig.java
@@ -3,6 +3,7 @@ package me.abeyta.it;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,7 +20,11 @@ public class ITTestConfig {
 	
 	@Bean(name="decksBaseUrl")
 	public URI itTestUrl() throws URISyntaxException {
-		return new URI("http://localhost:8080/decks");
+		String httpPort = System.getProperty("ittest.http.port");
+		if(StringUtils.isEmpty(httpPort)) {
+			return new URI("http://localhost:8080/decks");	
+		}
+		return new URI("http://localhost:" + httpPort + "/decks");
 	}
 	
 }
