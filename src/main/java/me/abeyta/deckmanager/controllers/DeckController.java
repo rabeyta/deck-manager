e0437a87fe6389a4f32e512bee85c42ac758fbc7
Robert Abeyta <robertabeyta@outlook.com>
2015-12-07 16:03:00 -0700

    removed exception handler from controller and added responseStatus annotation to exception..accomplishes the same thing and removes some unnecessary code / method

--- src/main/java/me/abeyta/deckmanager/controllers/DeckController.java
+++ src/main/java/me/abeyta/deckmanager/controllers/DeckController.java
@@ -5,7 +5,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -50,7 +49,4 @@ public class DeckController {
 		return manager.shuffle(deckName);
 	}
 	
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	@ExceptionHandler(DeckNotFoundException.class)
-	public void handleDeckNotFound() {}
 }
--- src/main/java/me/abeyta/deckmanager/exceptions/DeckNotFoundException.java
+++ src/main/java/me/abeyta/deckmanager/exceptions/DeckNotFoundException.java
@@ -1,5 +1,9 @@
 package me.abeyta.deckmanager.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class DeckNotFoundException extends Exception {
 
 	private static final long serialVersionUID = -3017733455403046961L;
