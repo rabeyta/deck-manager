1266b9e877b0aa4c79a48ff7dc29d232290226b4
Robert Abeyta <robertabeyta@outlook.com>
2015-12-06 17:40:36 -0700

    fixed error with hand shuffle. wasnt passing in shuffled cards each time. adjusted logic on number of shuffles to not allow deck resetting

--- src/main/java/me/abeyta/deckmanager/delegates/shuffle/HandShuffler.java
+++ src/main/java/me/abeyta/deckmanager/delegates/shuffle/HandShuffler.java
@@ -13,17 +13,33 @@ import me.abeyta.deckmanager.model.Deck;
 
 public class HandShuffler implements Shuffler {
 
+	/**
+	 * Hand shuffling technique that weaves a half of a deck over each other
+	 * 
+	 * https://en.wikipedia.org/wiki/Faro_shuffle
+	 */
 	@Override
 	public void shuffle(Deck deck) {
 		Card[] cards = convertDeckIntoCardArray(deck);
 
+		int numberOfShuffles = getRandomShuffleCountThatDoesntResetDeck();
+
 		Card[] shuffledCards = shuffleCards(cards);
-		
-		for(int x = 0; x < RandomUtils.nextInt(1, 100); x++) {
-			shuffledCards = shuffleCards(cards);
+		for(int x = 0; x < numberOfShuffles; x++) {
+			shuffledCards = shuffleCards(shuffledCards);
 		}
 
 		deck.setCards(convertCardArrayIntoSet(shuffledCards));
+	}
+
+	private int getRandomShuffleCountThatDoesntResetDeck() {
+		int numberOfShuffles = 0;
+		
+		do {
+			numberOfShuffles = RandomUtils.nextInt(1, 100) + 1; //account for previous shuffle already done
+		} while(numberOfShuffles % 8 == 0 || numberOfShuffles % 52 == 0); //ensure we do not reset the deck with an 8 or 52
+		
+		return numberOfShuffles;
 	}
 
 	private Card[] shuffleCards(Card[] cards) {
