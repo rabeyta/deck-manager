29fe03537c531813ba6394f328ace73d97f1dac6
Robert Abeyta <robertabeyta@outlook.com>
2015-12-12 08:38:10 -0700

    adding test for coverage of 8 and original order being returned

--- src/test/java/me/abeyta/deckmanager/delegates/shuffle/HandShufflerTest.java
+++ src/test/java/me/abeyta/deckmanager/delegates/shuffle/HandShufflerTest.java
@@ -1,5 +1,6 @@
 package me.abeyta.deckmanager.delegates.shuffle;
 
+import static me.abeyta.deckmanager.model.CardUtils.assertAllCardsArePresent;
 import static me.abeyta.deckmanager.model.CardUtils.isDeckInDefaultOrder;
 import static me.abeyta.deckmanager.model.CardUtils.isDeckSizeCorrect;
 import static org.junit.Assert.assertFalse;
@@ -13,20 +14,38 @@ import me.abeyta.deckmanager.model.Deck;
 public class HandShufflerTest {
 
 	private HandShuffler shuffler;
+	private Deck deck;
 	
 	@Before
 	public void setup() {
 		shuffler = new HandShuffler();
+		deck = getDeck();
 	}
 	
 	@Test
 	public void shuffleChangesOrder() {
-		Deck deck = new Deck();
-		assertTrue("Deck was not in default order", isDeckInDefaultOrder(deck));
-		
 		shuffler.shuffle(deck);
 		
+		validateDeck(deck);
+	}
+	
+	@Test
+	public void shuffleALotToEnsureDeckIsNeverRest() {
+		for(int x = 0; x < 10000; x++) {
+			shuffler.shuffle(deck);
+			validateDeck(deck);			
+		}
+	}
+
+	private void validateDeck(Deck deck) {
 		assertTrue("Deck was not right size", isDeckSizeCorrect(deck));
 		assertFalse("Deck was not shuffled", isDeckInDefaultOrder(deck));
+		assertAllCardsArePresent(deck);
+	}
+
+	private Deck getDeck() {
+		Deck deck = new Deck();
+		assertTrue("Deck was not in default order", isDeckInDefaultOrder(deck));
+		return deck;
 	}
 }
